f430cbc939f37cf50e6102335ae3dfd2fba06766
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-01-31 04:39:15 +0300

    some changes

--- src/threads/debug.sh
+++ src/threads/debug.sh
@@ -1,1 +1,1 @@
-gdb kernel.o -tui -ex 'target remote 0.0.0.0:1234' -ex 'b timer_interrupt' -ex 'list'
+pintos-gdb kernel.o -tui -ex 'set arch i386:x86-64' -ex 'target remote 0.0.0.0:1234' -ex 'b test_alarm_negative' -ex 'list'
